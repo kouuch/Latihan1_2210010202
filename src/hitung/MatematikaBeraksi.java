46d6c5d925c43d57a63dcdb3936a997533590076
kouuch <rifkyirsada2@gmail.com>
2024-06-04 22:49:24 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hitung;
 
-/**
- *
- * @author User
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //Membuat Objek
+        Matematika m1 = new Matematika(0,2);
+        
+        //menampilkan method
+        System.out.println("Hasil Penjumlahan"+m1.setPenjumlahan());
+        System.out.println("Hasil Pengurangan"+m1.setPengurangan());
+        System.out.println("Hasil Perkalian"+m1.setPerkalian());
+        System.out.println("Hasil Pembagian"+m1.setPembagian());
+    }
 }
